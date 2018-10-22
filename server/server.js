66a1e2cc2d9234c773df9b50adb791a352ea9924
BrianC <seahorse8789@gmail.com>
2018-10-21 19:44:17 -0700

    cleaned up files and fixed and issue with a div not rendering correctly.

--- server/server.js
+++ server/server.js
@@ -2,9 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv').config();
 const axios = require ("axios");
-
 const app = express();
-
 app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
